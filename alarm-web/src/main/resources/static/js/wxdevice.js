bc2b2d1c3e7fb2f8db0abb1f950bca0582dad7cc
杨佳楠 <553069938@qq.com>
2019-05-27 17:45:55 +0800

    修改绑定JS

--- alarm-web/src/main/resources/static/js/wxdevice.js
+++ alarm-web/src/main/resources/static/js/wxdevice.js
@@ -19,6 +19,71 @@ $(function() {
             alert("请输入用户名称");
             return;
         }
+        var user_mail = $("#user-mail").val();
+        if (user_mail.trim().length == 0) {
+            alert("请输入邮箱");
+            return;
+        }else{
+        	var myReg=/^[a-zA-Z0-9_-]+@([a-zA-Z0-9]+\.)+(com|cn|net|org)$/;
+        	if(!myReg.test(user_mail)){
+        		alert("邮箱格式不正确");
+                return;
+        	}
+        }
+        
+        var user_phone = $("#user-phone").val();
+        if (user_phone.trim().length == 0) {
+            alert("请输入手机号");
+            return;
+        }else{
+        	var myReg=/^[1][3,4,5,7,8,9][0-9]{9}$/;
+        	if(!myReg.test(user_phone)){
+        		alert("手机号不正确");
+                return;
+        	}
+        }
+        var time = $("#time").val();
+        if (time.trim().length == 0) {
+            alert("请输入日期");
+            return;
+        }else {
+        	var d = new Date(time);
+        	time = d.getTime() / 1000;
+        }
+        
+        var maxFlow = $("#maxFlow").val();
+        if (maxFlow.trim().length == 0) {
+            alert("请输入最大分析流量");
+            return;
+        }else{
+        	var re = /^[0-9]+.?[0-9]*/;
+        	if(!re.test(maxFlow)){
+        		alert("最大分析流量只能输入数字");
+                return;
+        	}
+        }
+        
+        var box = document.getElementsByName('checkbox')
+        var device_data = {};
+        var checkedIndex = 0; 
+        for(var i = 0; i <box.length; i++){
+        	if(box[i].checked){
+        		checkedIndex = 1;
+        		device_data[box[i].value] = 1;
+        	}else{
+        		device_data[box[i].value] = 0;
+        	}
+        }
+        
+        if(checkedIndex == 0 ){
+        	alert("请选择模块");
+            return;
+        }
+        device_data["macs"] = $("#user-macs").val();
+        device_data["cpus"] = $("#user-cpus").val();
+        device_data["maxFlow"] = parseInt(maxFlow);
+        device_data["validTerm"] = time;
+        
         var user_data = {
             account: user_account,
             password: password,
@@ -26,12 +91,8 @@ $(function() {
             mail: $("#user-mail").val(),
             phone: $("#user-phone").val(),
             wechat: $("#user-wechat").val(),
-            qq: $("#user-qq").val()
         };
-        var device_data = {
-            macs: $("#user-macs").val(),
-            cpus: $("#user-cpus").val()
-        };
+        
         createUserWithDevice(user_data, device_data);
     });
 
@@ -56,23 +117,10 @@ function createUserWithDevice(user_data, device_data) {
         success: function(data){
             if (data["code"] != 2000) {
                 alert("新建用户账号失败，原因:"+data["reason"]);
-            } 
-            userid = data["userid"];
-        }
-    });
-    device_data["userid"] = userid;
-    $.ajax({
-        url: "/addDevice",
-        type: "POST",
-        data: JSON.stringify(device_data),
-        dataType: "json",
-        contentType: "application/json;charset=utf-8",
-        success: function(data){
-            if (data["code"] != 2000) {
-                alert("新建设备失败，原因:"+data["reason"]);
             } else {
-                alert("新建用户设备成功！");
-            }
+            	 device_data["userid"] =  data["userid"];
+            	 bindDevice(device_data);
+            } 
         }
     });
 }
